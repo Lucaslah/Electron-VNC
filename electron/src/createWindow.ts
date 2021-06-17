8fe3523639a213c6f60edf427d5d78a9be15cb5a
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-06-17 22:55:49 +1200

    Update createWindow.ts

--- electron/src/createWindow.ts
+++ electron/src/createWindow.ts
@@ -1,1 +1,26 @@
-export default class createWindow {}
+import {
+    BrowserWindow
+} from "electron";
+import path from "path";
+
+export default class createWindow extends BrowserWindow {
+    private width: number;
+    private height: number;
+    private preload: string;
+    constructor(preload: string, { width, height }: { width: number, height: number }) {
+        super({
+            width: width,
+            height: height,
+            webPreferences: {
+                preload: path.join(__dirname, preload)
+            }
+        });
+        this.width = width;
+        this.height = height;
+        this.preload = preload;
+    }
+
+    public createloadURL(url: string): void {
+        this.loadURL(url); // 'file://' + __dirname + '/browser.html'
+    }
+}
